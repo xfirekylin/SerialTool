06572750e80516dd373e88ca11ca05f8754012d6
xiaolihui <xiaolihui@inricosolutions.com>
2023-11-23 10:46:45 +0800

    设置窗口大小和起始位置

--- SerialTool/source/main.cpp
+++ SerialTool/source/main.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
 #include <QtWidgets/QApplication>
+#include <QApplication>
+#include <QDesktopWidget>
+#include <QScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
     MainWindow w;
+    w.resize(1000,w.height());
+    QList<QScreen *> screens = QGuiApplication::screens();
+    QRect screenGeometry = screens.at(0)->geometry();
+    int x = (screenGeometry.width() - w.width()) / 2;
+    int y = (screenGeometry.height() - w.height()) / 2;
+    w.move(x, y);
+
     w.show();
 
     return a.exec();
